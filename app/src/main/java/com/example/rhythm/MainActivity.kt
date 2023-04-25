b26b27731b02b59ae0ee2c9e6c152b71f19222ab
unknown <gauravdhande110@gmail.com>
2023-04-26 01:52:00 +0530

    Add blank music list fragment
    Support Added:
    - Updated theme to NoActionBar to avoid action bar to be shown.
    - Added MusicListFragment and lauched from MainActivity.

--- app/src/main/java/com/example/rhythm/MainActivity.kt
+++ app/src/main/java/com/example/rhythm/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.rhythm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rhythm.ui.MusicListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        launchMusicListFragment()
+    }
+
+    private fun launchMusicListFragment() {
+        val musicListFragment = MusicListFragment.newInstance()
+        with(supportFragmentManager.beginTransaction()) {
+            add(R.id.fragment_container, musicListFragment)
+            commitNow()
+        }
     }
 }
